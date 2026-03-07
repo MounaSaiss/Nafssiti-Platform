758bd9909844786f177c8623d377a5051b0efcd4
LunaDevX <mouna1.saiss@gmail.com>
2026-03-07 21:25:58 +0000

    feat: Implement initial public web interface including home page, authentication entry points, and psychologist directory.

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,13 @@
 
 use App\Http\Controllers\auth\loginController;
 use App\Http\Controllers\auth\registerController;
-use Illuminate\Support\Facades\Route;
-use PHPUnit\Runner\HookMethod;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
-
-Route::get('/',[HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/login', [loginController::class, 'showLoginForm'])->name('login');
 Route::get('/register/user', [registerController::class, 'showRegistrationForm'])->name('register.user');
 Route::get('/register/psychologue', [registerController::class, 'showPsychologueRegistrationForm'])->name('register.psychologue');
-
-
+Route::get('/psychologues', [UserController::class, 'allPsychologues'])
+    ->name('psychologue.allPsychologues');
